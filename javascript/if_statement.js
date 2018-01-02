33874229dcaa8040ff38d0f726cfd13f22ef30c8
xiacijie <cijie@ualberta.ca>
2018-01-01 20:38:18 -0700

    if

---
+++ javascript/if_statement.js
@@ -0,0 +1,13 @@
+var number_to_guess = 4;
+
+var guess = prompt("Guess a number");
+if (number_to_guess == guess){
+	alert("True")
+}
+
+else{
+	alert("You are false")
+}
+
+// data cast
+Number(guess)

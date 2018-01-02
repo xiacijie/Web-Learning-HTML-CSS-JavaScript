6dda919ae05a5d88ec308d5e0e5467ef1e4a9e96
xiacijie <cijie@ualberta.ca>
2018-01-01 22:26:20 -0700

    obj

--- javascript/object/object.js
+++ javascript/object/object.js
@@ -5,3 +5,11 @@ var person{
 };
 alert(person["name"]);//or
 alert(person.name);
+
+//create objects
+var objec = {};
+//or
+var objec = new Object();
+objec.name = "Jack";
+objec.age = 19;
+objec.city = "Suzhou";
